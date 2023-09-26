979187191339b27eef61b330e87d300d844dc740
MotoZs <b1papzso@vasvari.org>
2023-09-26 08:14:26 +0200

    vegso

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ var osztaly = new szamologep();
 var osztaly2 = new szamologep2();
 
 while (true) { 
-    Console.WriteLine("ö - összeadás / k - kivonás / o - osztás / sz - szorzás");
+    Console.WriteLine("ö - összeadás / k - kivonás / o - osztás / sz - szorzás / szar - másodfokú");
     string c = Console.ReadLine();
 
+    /*
     if (c == "ö")
         osztaly.összeg();
     else if (c == "k")
@@ -19,7 +20,37 @@ while (true) {
         osztaly2.szorz();
     else if (c == "o")
         osztaly2.osztas();
+    else if (c == "szar")
+        osztaly.masodfok();
     else if (c == "ki")
         break;
+    */
+    switch (c)
+    {
+        case "ö":
+            osztaly.összeg();
+            break;
+
+        case "k":
+            osztaly.kivonas();
+            break;
+
+        case "sz":
+            osztaly2.szorz();
+            break;
+
+        case "o":
+            osztaly2.osztas();
+            break;
+
+        case "szar":
+            osztaly.masodfok();
+            break;
+
+        case "ki":
+            Environment.Exit(0);
+            break;
+    }   
+
     Console.WriteLine("");
 }
--- class_gyak.cs
+++ class_gyak.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,48 @@ namespace ClassWork0921
 
             Console.WriteLine(szam1 - szam2);
 
+        }
+        public void masodfok() 
+        {
+            while (true)
+            {
+            Console.WriteLine("Adj meg egy számot: ");
+            string szam1 = Console.ReadLine();
+            int a = int.Parse(szam1);
+            
+                if (a == 0)
+                {
+                    Console.WriteLine("Ez az érték nem lehet 0!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Adj meg egy újabb számot: ");
+                    string szam2 = Console.ReadLine();
+                    int b = int.Parse(szam2);
+
+                    Console.WriteLine("Adj meg egy újabb számot: ");
+                    string szam3 = Console.ReadLine();
+                    int c = int.Parse(szam3);
+
+                    if ((b * b) - 4 * a * c < 0) 
+                    {
+                        Console.WriteLine("A Diszkrimináns kisebb mint 0 -- NINCS Mo.");
+                        break;
+                    }
+                    else
+                    {
+                        double result1 = (double)((-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a));
+                        Console.WriteLine("Ennek a szarnak az eredménye: ");
+                        Console.WriteLine(result1);
+                        double result2 = (double)((-b - Math.Sqrt((b * b) - (4 * a * c))) / (2 * a));
+                        Console.WriteLine("Ennek a szarnak az eredménye: ");
+                        Console.WriteLine(result2);
+                        break;
+                    }
+                        
+                }
+            }
         }
     }
 }
